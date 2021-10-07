514d3d847b8283fae5ed7820d3e5d345f3a9d2e1
Louie <sky85245630@gmail.com>
2021-10-07 17:42:53 +0800

    createTodo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: "asd",
+        };
+        this.createTodo = this.createTodo.bind(this);
+    }
+
+    createTodo() {
+        this.props.createTodo(this.state.text);
+    }
+
     render() {
         return (
             <div>
                 <input />
-                <button>new</button>
+                {/* <button onClick={() => this.props.createTodo(this.state.text)}>
+                    new
+                </button> */}
+                <button onClick={() => this.createTodo()}>new</button>
             </div>
         );
     }
